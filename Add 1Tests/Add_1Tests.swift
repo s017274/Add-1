a9a5daee1569c7009e567943f6f4f556739f837f
Ben Warren <s017274@students.lmsd.org>
2020-06-09 14:11:51 -0400

    Initial Commit

---
+++ Add 1Tests/Add_1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Add_1Tests.swift
+//  Add 1Tests
+//
+//  Created by Benjamin Warren (student LM) on 5/22/20.
+//  Copyright © 2020 Benjamin Warren (student LM). All rights reserved.
+//
+
+import XCTest
+@testable import Add_1
+
+class Add_1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
